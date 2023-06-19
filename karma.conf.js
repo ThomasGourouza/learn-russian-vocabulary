8f61402079d86d57af84a2229d70fe26bc727d55
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-06-19 10:02:17 +0200

    update ci cd

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/deutsch'),
+      dir: require('path').join(__dirname, './coverage/russian'),
       subdir: '.',
       reporters: [
         { type: 'html' },
